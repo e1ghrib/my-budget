75de74adfbc787f73b28314ebe63a8309b9e8c6f
E1Ghrib <elghribabdelmoumen@gmail.com>
2026-04-16 17:20:41 +0100

    remove About component and its associated content

--- app/routes/about.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>About</h1>
-      <p>Welcome to the About page.</p>
-      <p>This is a simple page created with React.</p>
-    </div>
-  );
-}
